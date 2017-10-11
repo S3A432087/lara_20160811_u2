1d20b5ff4d532fb709b4914384177cc186d6da76
jay <h220641963@gmail.com>
2017-10-11 18:20:26 +0800

    單元二

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('hello/{name?}', function ($name = 'Everybody') {
+
+    return ' Hello,' .$name ;
+});
+
+Route::get('say/{name?}',['as'=>'hello.index',function($name=
+                                                       'Everybody'){
+    returnview('welcome');
+}]);
+
+
+Route::get('/',function(){
+    return redirect('welcome');
+});
+
+Route::get('Hello/{name?}',['as'=>'hello.index',function($name=
+                                                         'Everybody'){
+    return'Hello,'.$name;
+}]);
+
+Route::get('dashboard',function(){
+    return 'dashboard';
+});
+
+Route::group(['prefix'=>'admin'],function(){
+    Route::get('dashboard',function(){
+        return'admin dashboard';
+    });
+});
+
+Route::get('/',['as'=>'home.index','uses'=>
+    'HomeController@index']);
+
